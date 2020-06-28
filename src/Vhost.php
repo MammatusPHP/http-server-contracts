b310426cf5c79c010e76b89953bb5cd51ce83cea
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-06-29 00:18:28 +0200

    Initial code commit

---
+++ src/Vhost.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Mammatus\Http\Server\Configuration;
+
+interface Vhost
+{
+    public function port(): int;
+
+    public function name(): string;
+
+    public function webroot(): Webroot;
+}
---
+++ src/Webroot.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Mammatus\Http\Server\Configuration;
+
+interface Webroot
+{
+}
